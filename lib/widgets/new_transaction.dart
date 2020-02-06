5e53ce6a7213178527f3efded0a8a99426e3a39f
Darkestt <martinez.alex.9790@gmail.com>
2020-02-05 22:08:27 -0400

    46. Se agrega metodo que gestiona el Datepicker: despliegue, seleccion de fecha y actualizacion de Estado del Widget

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; //permite formatear la fecha
 
 class NewTransaction extends StatefulWidget {
   //Inputs
@@ -11,27 +12,51 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final tituloController = TextEditingController();
+  final _tituloController = TextEditingController();
+  final _montoController = TextEditingController();
+  DateTime _fechaSeleccionada;
 
-  final montoController = TextEditingController();
-
-  void submitData() {
-    print(tituloController
+  void _submitData() {
+    print(_tituloController
         .text); //se prueba que se esten recibiendo bien los datos de entrada
     //print(montoInput);
-    print(montoController
+    print(_montoController
         .text); //de esta forma se accede al contenido almacenado en la varible Controller
 
-    if (tituloController.text.isNotEmpty &&
-        tituloController.text != null &&
-        montoController.text != null) {
-      double monto = double.parse(montoController.text);
+    if (_tituloController.text.isNotEmpty &&
+        _tituloController.text != null &&
+        _montoController.text != null) {
+      double monto = double.parse(_montoController.text);
       if (monto > 0.0) {
-        widget.nuevaTransaccion(tituloController.text, monto);
+        widget.nuevaTransaccion(_tituloController.text, monto);
         //para cerrar el Modal al finalizar el insert
         Navigator.of(context).pop();
       }
     }
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+            context:
+                context, //context que viene incluido en el State de la clase
+            initialDate: DateTime
+                .now(), //fecha inicial que se mostrara al abrir el Datepicker
+            firstDate: DateTime(2019), //restriccion para fecha mas antigua
+            lastDate: DateTime
+                .now() //restriccion para fecha mas reciente o futura, en este caso no se permite fechas a futuro
+            )
+        .then((fechaSeleccionada) {
+      //funcion que se ejecuta despues de que el usuario selecciona una fecha o cancele
+      if (fechaSeleccionada == null) {
+        //si el usuario cancela retorna null
+        return;
+      }
+      setState(() {
+        //Actualizar el Widget
+        this._fechaSeleccionada =
+            fechaSeleccionada; //de seleccionar una fecha se setea la variable global
+      });
+    });
   }
 
   @override
@@ -51,7 +76,7 @@ class _NewTransactionState extends State<NewTransaction> {
                   labelText:
                       "Titulo"), //label para identificar el input al usuario
               //onChanged: (val) => tituloInput = val, //se asigna el valor del input a la variable, cada vez que presione una tecla
-              controller: tituloController,
+              controller: _tituloController,
             ),
             TextField(
               decoration: InputDecoration(labelText: "Monto"),
@@ -59,7 +84,7 @@ class _NewTransactionState extends State<NewTransaction> {
                       montoInput = valor;
                     },*/
               controller:
-                  montoController, //otra forma de obtener datos input, es utilizando controllers
+                  _montoController, //otra forma de obtener datos input, es utilizando controllers
               keyboardType: TextInputType
                   .number, //tipo de teclado a mostrar en pantalla cuando se hace focus
             ),
@@ -67,11 +92,20 @@ class _NewTransactionState extends State<NewTransaction> {
               height: 70,
               child: Row(
                 children: <Widget>[
-                  Text("No se ha seleccionado una Fecha."),
+                  //para formatear la fecha seleccionada
+                  Expanded(//para darle todo el espacio disponible al texto, darle solo lo necesario al boton manteniendolo en el extremo derecho de la pantalla
+                      child: Text(
+                    _fechaSeleccionada == null
+                        ? "No se ha seleccionado una Fecha."
+                        : "Fecha Seleccionada: ${DateFormat.yMd().format(_fechaSeleccionada)}",
+                  )),
                   FlatButton(
                     textColor: Theme.of(context).primaryColor,
-                    child: Text("Seleccionar Fecha", style: TextStyle(fontWeight: FontWeight.bold),),
-                    onPressed: () {},
+                    child: Text(
+                      "Seleccionar Fecha",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: _presentDatePicker,
                   ),
                 ],
               ),
@@ -80,7 +114,7 @@ class _NewTransactionState extends State<NewTransaction> {
               child: Text("Agregar Transacción"),
               color: Theme.of(context).primaryColor,
               textColor: Theme.of(context).textTheme.button.color,
-              onPressed: submitData,
+              onPressed: _submitData,
             ),
           ],
         ),
